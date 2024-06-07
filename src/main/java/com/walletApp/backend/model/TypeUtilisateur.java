df98daf939e0d8e3d15d2578261e0af0904d4df0
Votre Nom <votre@email.com>
2024-06-07 19:47:10 +0000

    bara

--- src/main/java/com/walletApp/backend/model/TypeUtilisateur.java
+++ src/main/java/com/walletApp/backend/model/TypeUtilisateur.java
@@ -5,8 +5,7 @@ import lombok.*;
 
 @Entity
 @Table(name = "type_utilisateurs")
-@Setter
-@Getter
+@Data  // Remplace @Setter et @Getter par @Data pour inclure également toString, equals et hashCode
 public class TypeUtilisateur {
 
     @Id
@@ -14,4 +13,12 @@ public class TypeUtilisateur {
     private int id_type_user;
 
     private String lib_type_user;
+
+    // Constructeur par défaut (généré par Lombok)
+    public TypeUtilisateur() {}
+
+    // Constructeur avec tous les champs (généré par Lombok)
+    public TypeUtilisateur(int id_type_user) {
+        this.id_type_user = id_type_user;
+    }
 }
